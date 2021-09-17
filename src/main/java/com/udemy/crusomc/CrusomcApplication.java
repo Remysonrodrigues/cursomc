cb0fff51213ffe641634759ecd19d3f6fcaa7a6f
remyson <remysonrodrigues@gmail.com>
2021-09-17 16:11:54 -0300

    Projeto criado

---
+++ src/main/java/com/udemy/crusomc/CrusomcApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.crusomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrusomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrusomcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/crusomc/CrusomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.crusomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrusomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
