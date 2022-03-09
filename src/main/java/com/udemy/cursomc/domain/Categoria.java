4e116e10b1588a90d53ef43108416e3d390c7c6f
Remyson <remysonrodrigues@gmail.com>
2022-03-09 15:24:14 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/udemy/cursomc/domain/Categoria.java
+++ src/main/java/com/udemy/cursomc/domain/Categoria.java
@@ -1,10 +1,9 @@
 package com.udemy.cursomc.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,20 +14,10 @@ public class Categoria implements Serializable {
     private Integer id;
     private String nome;
 
+    @ManyToMany(mappedBy = "categorias")
+    private List<Produto> produtos = new ArrayList<>();
+
     public Categoria() {}
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Categoria categoria = (Categoria) o;
-        return Objects.equals(id, categoria.id) && Objects.equals(nome, categoria.nome);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nome);
-    }
 
     public Categoria(Integer id, String nome) {
         super();
@@ -51,4 +40,26 @@ public class Categoria implements Serializable {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id) && Objects.equals(nome, categoria.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+
 }
---
+++ src/main/java/com/udemy/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.udemy.cursomc.repositories;
+
+import com.udemy.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
