d097039e6806dad1e539e18b179562352545e52e
Remyson <remysonrodrigues@gmail.com>
2022-03-10 11:46:30 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/udemy/cursomc/services/CategoriaService.java
+++ src/main/java/com/udemy/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.udemy.cursomc.services;
 
 import com.udemy.cursomc.domain.Categoria;
 import com.udemy.cursomc.repositories.CategoriaRepository;
+import com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,9 @@ public class CategoriaService {
     public Categoria buscar(Integer id) {
 
         Optional<Categoria> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+            "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()
+        ));
 
     }
 
