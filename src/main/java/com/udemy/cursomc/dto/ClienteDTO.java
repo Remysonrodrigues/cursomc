672dfe232feb0bf9df7e9cc876277a1bfa233142
Remyson <remysonrodrigues@gmail.com>
2022-04-23 11:15:37 -0300

    validacao customizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/udemy/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/udemy/cursomc/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.udemy.cursomc.dto;
 
 import com.udemy.cursomc.domain.Cliente;
+import com.udemy.cursomc.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
     private Integer id;
---
+++ src/main/java/com/udemy/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,52 @@
+package com.udemy.cursomc.services.validation;
+
+import com.udemy.cursomc.domain.Cliente;
+import com.udemy.cursomc.domain.enums.TipoCliente;
+import com.udemy.cursomc.dto.ClienteDTO;
+import com.udemy.cursomc.repositories.ClienteRepository;
+import com.udemy.cursomc.resources.exception.FieldMessage;
+import com.udemy.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public void initialize(ClienteUpdate ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+        @SuppressWarnings("unchecked")
+        Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        Integer uriId = Integer.parseInt(map.get("id"));
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+        if (aux != null && !aux.getId().equals(uriId)) {
+            list.add(new FieldMessage("email", "Email já existente"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
