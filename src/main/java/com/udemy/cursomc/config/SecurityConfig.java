624337d8893b3eb3c8575510b8393f7fd3e69b5d
Remyson <remysonrodrigues@gmail.com>
2022-04-28 17:05:49 -0300

    implementando autoriazacao

--- src/main/java/com/udemy/cursomc/config/SecurityConfig.java
+++ src/main/java/com/udemy/cursomc/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.udemy.cursomc.config;
 
 import com.udemy.cursomc.security.JWTAuthenticationFilter;
+import com.udemy.cursomc.security.JWTAuthorizationFilter;
 import com.udemy.cursomc.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .anyRequest()
             .authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
