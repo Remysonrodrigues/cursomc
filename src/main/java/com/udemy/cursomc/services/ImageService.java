42edaab456e442a96608ce8c3870cfd376e9f800
Remyson <remysonrodrigues@gmail.com>
2022-05-02 11:27:14 -0300

    Ajustando tamanho da imagem

--- src/main/java/com/udemy/cursomc/services/ImageService.java
+++ src/main/java/com/udemy/cursomc/services/ImageService.java
@@ -1,7 +1,9 @@
 package com.udemy.cursomc.services;
 
+import com.amazonaws.services.dynamodbv2.xspec.S;
 import com.udemy.cursomc.services.exceptions.FileException;
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -51,6 +53,21 @@ public class ImageService {
         } catch (IOException e) {
             throw new FileException("Erro ao ler o arquivo");
         }
+    }
+
+    public BufferedImage cropSquare(BufferedImage sourceImg) {
+        int min = Math.min(sourceImg.getHeight(), sourceImg.getWidth());
+        return Scalr.crop(
+            sourceImg,
+            (sourceImg.getWidth()/2) - (min/2),
+            (sourceImg.getHeight()/2) - (min/2),
+            min,
+            min
+        );
+    }
+
+    public BufferedImage resize(BufferedImage sourceImg, int size) {
+        return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
     }
 
 }
